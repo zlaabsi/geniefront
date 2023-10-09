c09b2c9b373797c43f9efa7d9f4cdd01cffacd66
unknown <ayoub.aarab.work@gmail.com>
2023-10-09 22:28:03 +0200

    Fix getting background image from static folder.

--- backend/app.py
+++ backend/app.py
@@ -147,7 +147,7 @@ def image_run():
     background_img_name = text_less_image(st.session_state.img)
     # background_image = f"http://localhost:8501/static/{background_img_name}"
     
-    background_image = f"https://geniefront.streamlit.app/static/{background_img_name}"
+    background_image = f"https://geniefront.streamlit.app/backend/static/{background_img_name}"
     if layout != []:
         html_code = html_css_gen(layout, background_image)
 
